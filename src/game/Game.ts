7d3024b772273be75cd001b5ac0e305f8ddd14ca
herman <hermanconnor11@gmail.com>
2025-06-10 23:03:48 -0400

    Create select difficulty method

--- src/game/Game.ts
+++ src/game/Game.ts
@@ -1,8 +1,8 @@
 import * as readline from 'node:readline/promises';
 import { stdin as input, stdout as output } from 'node:process';
+import chalk from 'chalk';
 import { GameConfig, GameState } from '../interfaces/gameInterfaces';
 import { Difficulty } from './difficulty';
-import chalk from 'chalk';
 
 export class Game {
   private rl: readline.Interface;
@@ -24,5 +24,61 @@ export class Game {
     console.log(
       'You have a limited number of chances to guess the correct number.',
     );
+  }
+
+  private async selectDifficulty(): Promise<GameConfig> {
+    console.log('\nPlease select the difficulty level:');
+    console.log('1. Easy (10 chances)');
+    console.log('2. Medium (5 chances)');
+    console.log('3. Hard (3 chances)');
+
+    let config: GameConfig;
+
+    while (true) {
+      const choice = await this.rl.question('Enter your choice: ');
+      const difficultyChoice = parseInt(choice);
+
+      switch (difficultyChoice) {
+        case Difficulty.Easy:
+          config = { maxChances: 10, hintThreshold: 7 };
+          console.log(
+            chalk.green(
+              '\nGreat! You have selected the Easy difficulty level.',
+            ),
+          );
+          break;
+
+        case Difficulty.Medium:
+          config = { maxChances: 5, hintThreshold: 3 };
+          console.log(
+            chalk.green(
+              '\nGreat! You have selected the Medium difficulty level.',
+            ),
+          );
+          break;
+
+        case Difficulty.Hard:
+          config = { maxChances: 3, hintThreshold: 1 };
+          console.log(
+            chalk.green(
+              '\nGreat! You have selected the Hard difficulty level.',
+            ),
+          );
+          break;
+
+        default:
+          console.log(
+            chalk.yellow('Invalid choice. Defaulting to Medium difficulty.'),
+          );
+          config = { maxChances: 5, hintThreshold: 3 };
+      }
+
+      break; // Valid choice, exit the loop
+    }
+
+    console.log(`You have ${config.maxChances} to guess the correct number.`);
+    console.log("Let's start the game!");
+
+    return config;
   }
 }
